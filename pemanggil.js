2d3d5f7cb5d1efff53e353affaf6214653f53d62
claudy7 <43817786+claudy7@users.noreply.github.com>
2018-10-04 08:31:37 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/10/hpv-dna-testing-detect-cervical-cancer.html"}
 if(x=="2"){xxx="/2017/10/ovarian-cancer-prognosis-and-benefits.html"}
 if(x=="3"){xxx="/2018/10/cervical-cancer-symptoms.html"}
 if(x=="4"){xxx="/2016/09/cervical-cancer.html"}
-if(x=="5"){xxx="/2014/10/food-for-ovarian-cancer-stage-3.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2014/10/food-for-ovarian-cancer-stage-3.html"}a_to_ve[i].href="https://streamingmovie-4k.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
